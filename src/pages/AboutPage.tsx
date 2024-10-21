4d2d7d96e59114cb45321ec950a4e5c0329450e8
Yun-Li <GCAM022101@coderacademy.edu.au>
2024-10-21 12:35:08 +1100

    pages

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage: React.FC = () => {
+    return (
+      <div>
+        <h2>About Us</h2>
+        <p>About us</p>
+      </div>
+    );
+  };
+
+export default AboutPage;

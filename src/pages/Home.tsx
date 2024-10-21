bd5eb81ca2d33f1ba56a081454da074fc544e6a5
Yun-Li <GCAM022101@coderacademy.edu.au>
2024-10-21 11:53:48 +1100

    Home Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to [按摩店名字]</h1>
+      <p>体验极致放松与舒适的按摩服务。我们提供专业的全身按摩、足疗、肩颈理疗等多种服务，帮助您缓解压力、恢复活力。</p>
+      <p>营业时间：每天上午 10 点至晚上 8 点</p>
+      <p>地址：[店铺地址]</p>
+      <p>联系我们：[电话号码]</p>w
+    </div>
+  );
+};
+
+export default Home;
